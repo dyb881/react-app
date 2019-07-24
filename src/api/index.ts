eb9d79ad531f7836ad2b72251355094c43bfc75d
dyb881 <15118995517@163.com>
2019-07-24 15:40:01 +0800

    优化类型注解

--- src/api/index.ts
+++ src/api/index.ts
@@ -4,5 +4,17 @@ import { post } from './request';
  * 用户
  */
 export const user = {
-  login: (data: any) => post('/login', data, '登录'),
+  login: (data: {
+    username: string;
+    password: string;
+  }): Promise<{
+    code: number;
+    msg: string;
+    data: {
+      name: string;
+      phone: number;
+      age: number;
+      [key: string]: any;
+    };
+  }> => post('/login', data, '登录'),
 };
--- src/api/mock-server.js
+++ src/api/mock-server.js
@@ -3,26 +3,12 @@ const ip = require('ip');
 
 console.log('模拟数据环境：', `http://localhost:3000/?host=http://${ip.address()}`);
 
-const tableInfo = {
-  id: '@id',
-  Batch: '@id',
-  Description: '@ctitle(50)',
-};
-
 // 数据统一返回处理
 mockServer(data => ({
   code: 0,
   msg: '模拟数据',
   data,
 }))
-  .get('/api/getTableList', req => {
-    const { pageSize = 10, pageNum = 1 } = req.query;
-    return {
-      [`list|${pageSize}`]: [tableInfo],
-      total: 100,
-      pageNum,
-    };
-  })
-  .get('/api/getTableInfo', tableInfo)
+  .get('/api/login', {})
   .delay(300, 1000) // 延迟时间
   .init(); // 启动服务
