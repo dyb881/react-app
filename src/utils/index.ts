9fa140351588dd02d85eadbab79ba68ecd9fe72b
dyb881 <15118995517@163.com>
2019-08-12 20:51:49 +0800

    更新依赖

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,15 +2,3 @@
  * 暂停
  */
 export const sleep = (outTime: number) => new Promise(r => setTimeout(r, outTime));
-
-/**
- * 根据键值列表获取一个新的对象
- */
-export const getKeysData = <T>(data: T, keys: (keyof T)[]) =>
-  keys.reduce(
-    (v, r) => {
-      v[r] = data[r];
-      return v;
-    },
-    {} as T
-  );
