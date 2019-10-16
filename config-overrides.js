c55ccc0b5540dc67c2509ddd6c6dea8dfc857343
dyb881 <15118995517@163.com>
2019-10-16 11:53:02 +0800

    更新依赖

--- config-overrides.js
+++ config-overrides.js
@@ -11,6 +11,7 @@ module.exports = override(
     javascriptEnabled: true,
     // 全局 less 变量，会覆盖项目内同名变量，可用于主题定制
     modifyVars: {
+      // 常用色
       '@ra-primary': '#1890ff', // 全局主色
       '@ra-success': '#52c41a', // 成功色
       '@ra-warning': ' #faad14', // 警告色
@@ -22,6 +23,20 @@ module.exports = override(
       '@ra-disabled-color-back': 'rgba(0, 0, 0, .05)', // 失效背景色
       '@ra-border-color': '#eeeeee', // 边框色
       '@ra-border-color-dark': '#cccccc', // 边框色-深色
+      '@ra-background': '#f1f1f1', // 背景色
+      // 别名
+      '@ra-p': '#1890ff', // 全局主色
+      '@ra-s': '#52c41a', // 成功色
+      '@ra-w': ' #faad14', // 警告色
+      '@ra-e': '#f5222d', // 错误色
+      '@ra-fs': '14px', // 主字号
+      '@ra-c': 'rgba(0, 0, 0, 0.85)', // 主文本色
+      '@ra-cs': 'rgba(0, 0, 0, .45)', // 次文本色
+      '@ra-dc': 'rgba(0, 0, 0, .25)', // 失效色
+      '@ra-dcb': 'rgba(0, 0, 0, .05)', // 失效背景色
+      '@ra-bc': '#eeeeee', // 边框色
+      '@ra-bcd': '#cccccc', // 边框色-深色
+      '@ra-b': '#f1f1f1', // 背景色
     },
   }),
   // 添加 postcss 插件 - 根据实际情况手动清除注释
