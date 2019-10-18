65da41f18b537629bfa41e35b36deb8630068116
dyb881 <15118995517@163.com>
2019-10-18 11:46:01 +0800

    清除压缩插件

--- config-overrides.js
+++ config-overrides.js
@@ -55,35 +55,12 @@ module.exports = override(
 
     if (process.env.NODE_ENV === 'production') {
       // 生产模式下的配置
-      const ParallelUglifyPlugin = require('webpack-parallel-uglify-plugin');
       config = Merge(config, {
         output: {
           publicPath: '.', // 引用脚本前缀
         },
         optimization: {
-          minimizer: [
-            // 多进程压缩
-            new ParallelUglifyPlugin({
-              // 缓存压缩后的结果
-              cacheDir: '.cache/',
-              uglifyJS: {
-                output: {
-                  // 最紧凑的输出
-                  beautify: false,
-                  // 删除所有的注释
-                  comments: false,
-                },
-                compress: {
-                  // 删除所有的 `console` 语句，可以兼容ie浏览器
-                  drop_console: true,
-                  // 内嵌定义了但是只用到一次的变量
-                  collapse_vars: true,
-                  // 提取出出现多次但是没有定义成变量去引用的静态值
-                  reduce_vars: true,
-                },
-              },
-            }),
-          ],
+          minimizer: [],
         },
       });
     } else {
