b1c4b315edabd1eb985b06e73d2ba3fc7bf1222e
dyb881 <15118995517@163.com>
2019-11-13 09:54:23 +0800

    更新依赖

--- src/types/type.ts
+++ src/types/type.ts
@@ -1,18 +1,4 @@
 /**
- * 必填
- */
-export type TRequired<T extends object> = {
-  [K in keyof T]-?: T[K];
-};
-
-/**
- * 非必填
- */
-export type TNotRequired<T extends object> = {
-  [K in keyof T]+?: T[K];
-};
-
-/**
  * 输入文本框
  */
 export type TInput<T = any> = {
@@ -23,4 +9,4 @@ export type TInput<T = any> = {
 /**
  * 输入文本框 - 非必填
  */
-export type TInputNotRequired<T = any> = TNotRequired<TInput<T>>;
+export type TInputNotRequired<T = any> = Partial<TInput<T>>;
