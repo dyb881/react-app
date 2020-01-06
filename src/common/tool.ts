3e4684cde67b79a77c976ec85485550284fcdb17
dyb881 <15118995517@163.com>
2020-01-06 09:53:33 +0800

    优化 toOptions tool

--- src/common/tool.ts
+++ src/common/tool.ts
@@ -2,7 +2,11 @@
  * 转为选项值
  * ['v1','v2'] => [{value: 1, label: 'v1'}, {value: 2, label: 'v2'}]
  */
-export const toOptions = (options: any[] | any) =>
-  Object.keys(options).map((k: any) =>
-    typeof options[k] === 'object' ? options[k] : { label: options[k], value: +k || +k === 0 ? +k : k }
-  );
+export const toOptions = (options?: any) => {
+  const isArray = Array.isArray(options);
+  return Object.keys(options || []).map((k: any) => {
+    const option = options[k];
+    if (typeof option === 'object') return option;
+    return { label: option, value: isArray ? +k : k };
+  });
+};
