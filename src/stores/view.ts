e5f4686e141b511af4eb019865e84c652932e944
dyb881 <15118995517@163.com>
2020-02-13 18:38:02 +0800

    优化设置标题

--- src/stores/view.ts
+++ src/stores/view.ts
@@ -10,5 +10,9 @@ export default class View {
   @observable title = window.document.title;
   @action setTitle = (title: string) => {
     window.document.title = this.title = title;
+    const iframe = document.createElement('iframe');
+    iframe.style.display = 'none';
+    iframe.onload = () => setTimeout(iframe.remove, 0);
+    document.body.removeChild(iframe);
   };
 }
