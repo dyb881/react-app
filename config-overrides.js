30e3b4edfe32f898f3c867c2a01e915dc868c87e
dyb881 <15118995517@163.com>
2020-04-28 15:46:34 +0800

    更新依赖

--- config-overrides.js
+++ config-overrides.js
@@ -20,21 +20,23 @@ module.exports = override(
   addBabelPlugin('react-hot-loader/babel'),
   // 添加 less-loader
   addLessLoader({
-    javascriptEnabled: true,
-    // 全局 less 变量，会覆盖项目内同名变量，可用于主题定制
-    modifyVars: {
-      '@ra-p': '#1890ff', // 全局主色
-      '@ra-s': '#52c41a', // 成功色
-      '@ra-w': '#faad14', // 警告色
-      '@ra-e': '#f5222d', // 错误色
-      '@ra-fs': '16px', // 主字号
-      '@ra-c': 'rgba(0, 0, 0, 0.85)', // 主文本色
-      '@ra-cs': 'rgba(0, 0, 0, .45)', // 次文本色
-      '@ra-dc': 'rgba(0, 0, 0, .25)', // 失效色
-      '@ra-dcb': 'rgba(0, 0, 0, .05)', // 失效背景色
-      '@ra-bc': '#eee', // 边框色
-      '@ra-bcd': '#ccc', // 边框色-深色
-      '@ra-b': '#fff', // 背景色
+    lessOptions: {
+      javascriptEnabled: true,
+      // 全局 less 变量，会覆盖项目内同名变量，可用于主题定制
+      modifyVars: {
+        '@ra-p': '#1890ff', // 全局主色
+        '@ra-s': '#52c41a', // 成功色
+        '@ra-w': '#faad14', // 警告色
+        '@ra-e': '#f5222d', // 错误色
+        '@ra-fs': '16px', // 主字号
+        '@ra-c': 'rgba(0, 0, 0, 0.85)', // 主文本色
+        '@ra-cs': 'rgba(0, 0, 0, .45)', // 次文本色
+        '@ra-dc': 'rgba(0, 0, 0, .25)', // 失效色
+        '@ra-dcb': 'rgba(0, 0, 0, .05)', // 失效背景色
+        '@ra-bc': '#eee', // 边框色
+        '@ra-bcd': '#ccc', // 边框色-深色
+        '@ra-b': '#fff', // 背景色
+      },
     },
   }),
   // 允许二次配置 eslint
