a5b532e6af21eb31c48a5769a200fceee405bd68
dyb881 <15118995517@163.com>
2020-07-18 16:14:35 +0800

    更新依赖

--- src/utils/regular.ts
+++ src/utils/regular.ts
@@ -1,3 +1,6 @@
+const carNumber = /^[京津沪渝冀豫云辽黑湘皖鲁新苏浙赣鄂桂甘晋蒙陕吉闽贵粤青藏川宁琼使领]{1}[A-HJ-NP-Z]{1}[A-HJ-NP-Z0-9]{4}[A-HJ-NP-Z0-9挂学警港澳]{1}$/;
+const carNumberD = /^[京津沪渝冀豫云辽黑湘皖鲁新苏浙赣鄂桂甘晋蒙陕吉闽贵粤青藏川宁琼使领]{1}[A-HJ-NP-Z]{1}(([0-9]{5}[DF])|([DF][A-HJ-NP-Z0-9][0-9]{4}))$/;
+
 /**
  * 正则
  */
@@ -5,7 +8,11 @@ export const regular = {
   /**
    * 车牌号
    */
-  carNumber: /^(?:[京津沪渝冀豫云辽黑湘皖鲁新苏浙赣鄂桂甘晋蒙陕吉闽贵粤青藏川宁琼使领 A-Z]{1}[A-HJ-NP-Z]{1}(?:(?:[0-9]{5}[DF])|(?:[DF](?:[A-HJ-NP-Z0-9])[0-9]{4})))|(?:[京津沪渝冀豫云辽黑湘皖鲁新苏浙赣鄂桂甘晋蒙陕吉闽贵粤青藏川宁琼使领 A-Z]{1}[A-Z]{1}[A-HJ-NP-Z0-9]{4}[A-HJ-NP-Z0-9 挂学警港澳]{1})$/,
+  carNumber: {
+    test(text: string) {
+      return carNumber.test(text) || carNumberD.test(text);
+    },
+  },
 
   /**
    * 手机号
